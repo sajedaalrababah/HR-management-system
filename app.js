6731c801c01768a88c153099cc8d2eae79fd69f8
sajedaalrababah <sajedaalrababah@icloud.com>
2023-03-07 18:58:41 +0000

    db

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ function Employee(fullName, department, level, img) {
     this.department = department;
     this.level = level;
     this.img = img;
-    this.netSalary = 0;
+    this.Salary = 0;
    
     employeeArr.push(this);
 }
@@ -30,20 +30,31 @@ Employee.prototype.salaryLevel= function() {
        }
    
      let rand=Math.floor(Math.random() * (max - min) ) + min;
-     this.netSalary=rand-(rand*.075);
-     return this.netSalary;
+     this.Salary=rand-(rand*.075);
+     return this.Salary;
 
       }
 
 
       Employee.prototype.render= function(){
 
-        document.write(`${this.fullName}&&${this.netSalary}`)
-
-
+        document.write(`${this.fullName}&&${this.Salary}`);
+        Ghazi.render();
+        Lana.render();
+        Tamara.render();
+        Safi.render();
+        Omar.render();
+        Rana.render();
+        Hadi.render();
 
 
 
       }
 
-
+      let Ghazi =new Employee ("1000","Ghazi Samer","Administration","img","level");
+      let Lana =new Employee ("1001","Lana Ali","Finance","img","level");
+      let Tamara=new Employee ("1002","Tamara Ayoub","Marketing","img","level");
+      let Safi=new Employee ("1003","Safi Walid","Administration","img","level");
+      let Omar =new Employee ("1004","Omar Zaid","Development","img","level");
+      let Rana=new Employee ("1005","Rana Saleh","Development","img","level");
+      let Hadi =new Employee ("1006","Hadi Ahmad","Finance","img","level");
