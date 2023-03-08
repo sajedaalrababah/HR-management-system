07d393d41a1ec5ca1e0682d637c4208266c41aab
sajedaalrababah <sajedaalrababah@icloud.com>
2023-03-08 17:11:58 +0000

    dmna

--- app.js
+++ app.js
@@ -6,20 +6,18 @@ function Employee(employeeId,fullName, department, level, img=null) {
     this.department = department;
     this.level = level;
     this.img = img;
-    this.salary=0;
+    
    
     employeeArr.push(this);
 }
-function salaryyy(min,max) {
-    let rand=Math.floor(Math.random() * (max - min) ) + min;
-     this.salary=rand-(rand*0.075);}
-
-Employee.prototype.salaryCla= function() {
-
-       if ( this.level=="senior"){
+    
+    
+  Employee.prototype.salaryCla=function(){
+    let min;
+    let max;
+       if ( this.level=="Senior"){
         min=1500;
         max=2000;
-        
        }
        
        else if(this.level=="Mid-senior"){
@@ -32,7 +30,9 @@ Employee.prototype.salaryCla= function() {
         max=1000;
         
        }
-   
+
+       let rand=Math.floor(Math.random() * (max - min) ) + min;
+       this.salary=rand-(rand*0.075);
        return this.salary;
      
 
@@ -41,21 +41,21 @@ Employee.prototype.salaryCla= function() {
 
       Employee.prototype.render= function(){
 
-        document.write(`employee name:${this.fullName}`)
-      document.write(`employee salary:${this.salary}`);
+        document.write(`employee name:${this.fullName}</br>`)
+        document.write(`employee salary:${this.salary}<br>`);
        
 
 
 
       }
-  employeeArr =[
+      employeeArr =[
      new Employee (1000,"Ghazi Samer","Administration","Senior"),
       new Employee (1001,"Lana Ali","Finance","Senior"),
       new Employee (1002,"Tamara Ayoub","Marketing","Senior"),
       new Employee (1003,"Safi Walid","Administration","Mid-senior"),
       new Employee (1004,"Omar Zaid","Development","Senior"),
       new Employee (1005,"Rana Saleh","Development","Junior"),
-    new Employee (1006,"Hadi Ahmad","Finance","Mid-senior")];
+      new Employee (1006,"Hadi Ahmad","Finance","Mid-senior")];
     //   Ghazi.render();
     //   Lana.render();
     //   Tamara.render();
@@ -67,4 +67,4 @@ Employee.prototype.salaryCla= function() {
     for( let i=0;i<employeeArr.length;i++){
         employeeArr[i].salaryCla();
         employeeArr[i].render();
-    }
+    };
