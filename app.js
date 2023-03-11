a9ad697c26954d221ef67b63c7dc41469bef6723
sajedaalrababah <sajedaalrababah@icloud.com>
2023-03-11 17:25:28 +0000

    k

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 'use strict';
 let employeeArr=[];
-function Employee(employeeId,fullName, department, level, img=null) {
+function Employee(employeeId,fullName, department, level, img) {
     this.employeeId =employeeId;
     this.fullName = fullName;
     this.department = department;
@@ -39,23 +39,26 @@ function Employee(employeeId,fullName, department, level, img=null) {
       }
 
 
-      Employee.prototype.render= function(){
+    //  Employee.prototype.render= function(){
 
-        document.write(`employee name:${this.fullName}</br>`)
-        document.write(`employee salary:${this.salary}<br>`);
+        //document.write(`employee name:${this.fullName}</br>`)
+        //document.write(`employee salary:${this.salary}<br>`);
+    
+    //}
+    
        
 
 
 
-      }
+      
       employeeArr =[
-     new Employee (1000,"Ghazi Samer","Administration","Senior"),
-      new Employee (1001,"Lana Ali","Finance","Senior"),
-      new Employee (1002,"Tamara Ayoub","Marketing","Senior"),
-      new Employee (1003,"Safi Walid","Administration","Mid-senior"),
-      new Employee (1004,"Omar Zaid","Development","Senior"),
-      new Employee (1005,"Rana Saleh","Development","Junior"),
-      new Employee (1006,"Hadi Ahmad","Finance","Mid-senior")];
+     new Employee (1000,"Ghazi Samer","Administration","Senior","../assets/Ghazi.jpg"),
+      new Employee (1001,"Lana Ali","Finance","Senior","../assets/Lana.jpg"),
+      new Employee (1002,"Tamara Ayoub","Marketing","Senior","../assets/Tamara.jpg"),
+      new Employee (1003,"Safi Walid","Administration","Mid-senior","../assets/Safi.jpg"),
+      new Employee (1004,"Omar Zaid","Development","Senior","../assets/Omar.jpg"),
+      new Employee (1005,"Rana Saleh","Development","Junior",'../assets/Rana.jpg'),
+      new Employee (1006,"Hadi Ahmad","Finance","Mid-senior","../assets/Hadi.jpg")];
     //   Ghazi.render();
     //   Lana.render();
     //   Tamara.render();
@@ -64,7 +67,72 @@ function Employee(employeeId,fullName, department, level, img=null) {
     //   Rana.render();
     //   Hadi.render();
 
+   // for( let i=0;i<employeeArr.length;i++){
+       // employeeArr[i].salaryCla();
+        //employeeArr[i].render();
+   // };
+
+
+
+    let body = document.getElementsByTagName("body");
+    let sectionEl =document.getElementById("sec");
+    let employeeForm = document.getElementById("employeeForm");
+    employeeForm.addEventListener('submit', employeeHandler);
+    function employeeHandler(event) {
+        event.preventDefault();
+        let fullname = event.target.fullname.value;
+        let departmentSelect = event.target.departmentSelect.value;
+        let levelselect = event.target.levelselect.value;
+        let imgPath = event.target.imgUrl.value;
+        let newEmployee = new Employee(fullname, departmentSelect, levelselect, imgPath);
+        console.log(newEmployee);
+    }
+
+
+        let id = 1000;
+        Employee.prototype.uniqueId = function () {
+            this.employeeId = id++;
+            return this.employeeId ;
+        }
+        
+        Employee.prototype.render= function(){
+        let divEl = document.getElementById('cards');
+       
+        let imgEl = document.createElement('img');
+        imgEl.src=this.img;
+        sectionEl.appendChild(imgEl);
+       
+
+        let nameEl = document.createElement('h3');
+        nameEl.textContent = (`Name:${this.fullName}`);
+        sectionEl.appendChild(nameEl);
+        
+      
+        let idEl =  document.createElement('p');
+        idEl.textContent = (`Id:${this.employeeId}`);
+        sectionEl.appendChild( idEl);
+
+
+
+       let departmentEl = document.createElement('p');
+        departmentEl.textContent = (`Department:${this.department}-level:${this.level} `);
+        sectionEl.appendChild(departmentEl);
+    
+       
+       
+        
+        
+        imgEl.width = "150";
+        imgEl.height = "150";
+     
+        
+    
+       
+        
+    
+    }
     for( let i=0;i<employeeArr.length;i++){
         employeeArr[i].salaryCla();
         employeeArr[i].render();
+        employeeArr[i].uniqueId();
     };
