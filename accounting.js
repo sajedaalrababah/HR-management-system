f97a6740649996d3585da50a46962ac3ed358501
sajedaalrababah <sajedaalrababah@icloud.com>
2023-03-18 18:59:30 +0000

    sdd '
    git commit -msdd

--- accounting.js
+++ accounting.js
@@ -1,77 +1,92 @@
-'use strict'
-
-let acc = localStorage.getItem("info");
-let tableEl =document.getElementById("table");
-
-let objArr= JSON.parse(acc);
-
-console.log(localStorage);
+'use srtict';
 
 
 
+let retreivedArr = localStorage.getItem('employe');
+let arrayData = JSON.parse(retreivedArr);
 
+const departments = {};
 
+for (let i = 0; i <arrayData.length; i++) {
+  const employee = arrayData[i];
+  
+  if (!departments[employee.department]) {
+    departments[employee.department] = {
+      count: 0,
+      totalSalary: 0,
+      averageSalary: 0
+    };
+  }
 
-    let trEl=document.createElement('tr');
-    tableEl.appendChild(trEl);
-
-    let depName = document.createElement("td");
-    depName.textContent = this.department
-    trEl.appendChild(depName);
-
-    let employeCountEl = document.createElement("td");
-    employeCountEl.textContent = this.employeesNum;
-    trEl.appendChild(employeCountEl);
-
-
-    let totSalaryEl = document.createElement("td");
-    totSalaryEl.textContent = this.totalSalary;
-    trEl.appendChild(totSalaryEl);
-
-    let avgSalaryEl = document.createElement("td");
-    avgSalaryEl.textContent = this.averge;
-    trEl.appendChild(avgSalaryEl);
-
-
-    new Employee(depName, employeCountEl, totalSalary, avgSalary);
- allDepartments.renderTable() ;
-
-
-
-
-
-function avgSalary() {
-    let newSalary = 0;
-    let avg = 0;
-    for (let i = 0; i < allDepartments.length; i++) {
-        newSalary += this.salary;
-    }
-    avg = newSalary/allDepartments.length;
-    return avg;
-    }
-
-
-
-    function totalSalary (){
-        let sum=0
-        for(let i=0; i<allDepartments.length;i++); {
-        sum+=allDepartments[i].totSalaryEl;
-    }
-    
-   
-      return sum;  
+  departments[employee.department].count++;
+  departments[employee.department].totalSalary += employee.salary;
 }
 
-
-
-
-
-function renderall(){
-    for( let i=0;i<allDepartments.length;i++){
-       
-        allDepartments[i].renderTable();
-        
-    }
-
+for (const department in departments) {
+  departments[department].averageSalary = departments[department].totalSalary / departments[department].count;
 }
 
+for (const department in departments) {
+
+
+const tableBody = document.getElementById("table-body");  
+    if (!department) {
+      continue;
+    }
+  const row = document.createElement("tr");
+  const td1 = document.createElement("td");
+  td1.textContent = department;
+  row.appendChild(td1);
+  const td2 = document.createElement("td");
+  td2.textContent = departments[department].count;
+  row.appendChild(td2);
+  const td3 = document.createElement("td");
+  td3.textContent = departments[department].totalSalary;
+  row.appendChild(td3);
+  const td4 = document.createElement("td");
+  td4.textContent = departments[department].averageSalary;
+  row.appendChild(td4);
+  tableBody.appendChild(row);
+ 
+}
+function totalSalary(){
+  let totalSalary = 0;
+for (const department in departments) {
+  totalSalary += departments[department].totalSalary;
+}
+return totalSalary;
+}
+
+function totalNum(){
+  let sum = 0;
+for (const department in departments) {
+  sum += departments[department].count;
+}
+return sum;
+}
+
+function totalAverage(){
+  let avg=0;
+for (const department in departments) {
+  avg+=departments[department].averageSalary
+}
+return avg;
+}
+
+const tableFooter = document.getElementById("table-footer");
+
+const td5 = document.createElement("td");
+td5.textContent = "Total";
+tableFooter.appendChild(td5);
+
+const td6 = document.createElement("td");
+td6.textContent = totalNum();
+tableFooter.appendChild(td6);
+
+const td7 = document.createElement("td");
+td7.textContent = totalSalary();
+tableFooter.appendChild(td7);
+
+const td8 = document.createElement("td");
+td8.textContent = totalAverage();
+tableFooter.appendChild(td8);
